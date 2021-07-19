cb07df194d9300dab0103d2820abc33b2b6518d3
andrzej-szafranski <52956003+andrzej-szafranski@users.noreply.github.com>
2021-07-19 08:07:33 +0200

    DB2 Connector added

---
+++ src/main/java/com/s4b/core/DB2Conector.java
@@ -0,0 +1,7 @@
+package com.s4b.core;
+
+public class DB2Conector extends CoreConnection {
+    public DB2Conector(String name) {
+        super(name);
+    }
+}
--- src/main/java/com/s4b/gradleTester2.java
+++ src/main/java/com/s4b/gradleTester2.java
@@ -1,11 +1,12 @@
 package com.s4b;
 
 import com.s4b.core.CoreConnection;
+import com.s4b.core.DB2Conector;
 
 public class gradleTester2 {
     public static void main(String[] str) {
         System.out.println("Hello");
-        CoreConnection c = new CoreConnection("Ambiwalent");
+        CoreConnection c = new DB2Conector("Ambiwalent");
         System.out.println(c.getName());
         System.out.println("End ..................");
     }
