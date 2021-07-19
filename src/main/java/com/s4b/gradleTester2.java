6bdd49d742523867692655ea2ce0ca6204907ff6
andrzej-szafranski <52956003+andrzej-szafranski@users.noreply.github.com>
2021-07-19 08:33:04 +0200

    Update gradleTester2.java
    Start added

--- src/main/java/com/s4b/gradleTester2.java
+++ src/main/java/com/s4b/gradleTester2.java
@@ -5,9 +5,11 @@ import com.s4b.core.DB2Conector;
 
 public class gradleTester2 {
     public static void main(String[] str) {
+        System.out.println("Start ..................");
         System.out.println("Hello");
         CoreConnection c = new DB2Conector("Ambiwalent");
         System.out.println(c.getName());
         System.out.println("End ..................");
+        
     }
 }
