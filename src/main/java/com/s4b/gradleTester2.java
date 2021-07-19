3b82188b3fe67c9ee35055bc1840f97fa2b9e3e8
andrzej-szafranski <52956003+andrzej-szafranski@users.noreply.github.com>
2021-07-19 07:45:16 +0200

    Chnage in .gitignore, new Connector

--- src/main/java/com/s4b/gradleTester2.java
+++ src/main/java/com/s4b/gradleTester2.java
@@ -1,7 +1,12 @@
 package com.s4b;
 
+import com.s4b.core.CoreConnection;
+
 public class gradleTester2 {
     public static void main(String[] str) {
         System.out.println("Hello");
+        CoreConnection c = new CoreConnection("Ambiwalent");
+        System.out.println(c.getName());
+        System.out.println("End ..................");
     }
 }
